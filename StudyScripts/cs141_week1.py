64367d4bb6b41c53b733330db13cf601e4e6dc92
Marcos Leandro <dimitridawn@gmail.com>
2019-04-25 17:51:57 -0700

    Cleaning up repo

--- StudyScripts/cs141_week1.py
+++
@@ -1,6 +0,0 @@
-#! /usr/bin/env python
-
-import webbrowser
-webbrowser.open('https://www.cs.ucr.edu/~stelo/cs141spring19/cs141Spring19_part1_analysis.pdf')
-webbrowser.open('https://www.cs.ucr.edu/~stelo/cs141spring19/hw1.pdf')
-webbrowser.open('https://www.cs.ucr.edu/~stelo/cs141spring19/hw1soln.pdf')
