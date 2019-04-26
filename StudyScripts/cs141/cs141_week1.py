ef29dfcbb281c6f54dfe2bb5e2d3ea7b5327fe77
AlisonWonderland <AlisonWonderland@github.com>
2019-04-25 17:57:18 -0700

    Week1 thru 3 material scripts

---
+++ StudyScripts/cs141/cs141_week1.py
@@ -0,0 +1,6 @@
+#! /usr/bin/env python
+
+import webbrowser
+webbrowser.open('https://www.cs.ucr.edu/~stelo/cs141spring19/cs141Spring19_part1_analysis.pdf')
+webbrowser.open('https://www.cs.ucr.edu/~stelo/cs141spring19/hw1.pdf')
+webbrowser.open('https://www.cs.ucr.edu/~stelo/cs141spring19/hw1soln.pdf')
---
+++ StudyScripts/cs141/cs141_week2.py
@@ -0,0 +1,7 @@
+#! /usr/bin/env python
+
+import webbrowser
+webbrowser.open('https://www.cs.ucr.edu/~stelo/cs141spring19/cs141Spring19_part1_analysis.pdf')
+webbrowser.open('https://www.cs.ucr.edu/~stelo/cs141spring19/cs141Spring19_part2_divide_conquer.pdf')
+webbrowser.open('https://www.cs.ucr.edu/~stelo/cs141spring19/hw2.pdf')
+webbrowser.open('https://www.cs.ucr.edu/~stelo/cs141spring19/hw2soln.pdf')
