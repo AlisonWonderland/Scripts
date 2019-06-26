2bb708ea2393f2fb4e336d5c842c876e2c979dce
AlisonWonderland <AlisonWonderland@github.com>
2019-06-26 15:07:09 -0700

    Testing push

---
+++ webscrapers/github_scrapper/webscraping_example/github_scraper.py
@@ -0,0 +1,37 @@
+#! /usr/bin/env python
+
+
+from selenium import webdriver 
+#To launch browser
+from selenium.webdriver.common.by import By 
+#Helps us find things on a page. Like a login
+from selenium.webdriver.support.ui import WebDriverWait 
+#To wait for the page to load.
+from selenium.webdriver.support import expected_conditions as EC 
+#Makes sure that page has loaded by looking at the the thing we specify to look at.
+from selenium.common.exceptions import TimeoutException
+#For handling timeouts
+
+# Specifying incognito mode as you launch your browser[OPTIONAL]
+option = webdriver.ChromeOptions()
+option.add_argument("--incognito")
+
+browser = webdriver.Chrome(executable_path='/mnt/d/Users/dimitri/Desktop/chromedriver.exe', chrome_options=option)
+
+# Go to desired website
+browser.get("https://github.com/AlisonWonderland")
+
+# Wait 20 seconds for page to load
+timeout = 20
+try:
+    # Wait until the final element [Avatar link] is loaded.
+    # Assumption: If Avatar link is loaded, the whole page would be relatively loaded because it is among
+    # the last things to be loaded.
+    WebDriverWait(browser, timeout).until(EC.visibility_of_element_located((By.XPATH, "//img[@class='avatar width-full height-full avatar-before-user-status']")))
+except TimeoutException:
+    print("Timed out waiting for page to load")
+    browser.quit()
+
+# Get the titles
+first_tile = browser.find_element_by_xpath("//span[@class='repo js-pinnable-item'][1]")
+print(type(first_tile))
