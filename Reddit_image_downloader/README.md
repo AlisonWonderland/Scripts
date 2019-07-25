4da24216c2af768ae48ef1e1059c8eeb83ad1480
AlisonWonderland <dimitridawn@gmail.com>
2019-07-24 21:26:48 -0700

    Script can download imgur albums now

--- Reddit_image_downloader/README.md
+++ Reddit_image_downloader/README.md
@@ -1,8 +1,35 @@
 # Reddit image downloader
-Script that currently downloads the "hottest" 5 pictures from the pics subreddit, r/pics.
+Script that currently downloads images from the first 5 posts in 'hot' from a subreddit and stores them in a folder called 'test'. It can download images that are directly linked by the post and also download images from imgur albums. It uses PRAW, a Reddit API wrapper, and imgurpython, a python client for the Imgur api, to download Imgur albums. 
+
+To specify the subreddit you wan to download images from, insert the name in the empty double quotes in this line of code in the 'main' function:
+```python
+subreddit = reddit.subreddit("")
+```
 
 Note: You may not be able to view gifv and mpv files depending on your OS and media players.
+Note: Imgurpython is no longer supported. I plan on changing the script to adapt to the new api.
 
 ## Instructions to run script
+Since this script requires it to be registered through a Reddit and Imgur, you have to have an account for both websites and register the script at:
+```
+https://api.imgur.com/oauth2/addclient
+https://www.reddit.com/prefs/apps/
+```
+
+Install praw and imgurpython:
+```
+pip install imgurpython
+pip install --upgrade praw
+```
+
+Create a praw.ini file, use this:
+https://praw.readthedocs.io/en/latest/getting_started/configuration/prawini.html
+
+Include the praw.ini file in the same directory as your script file.
+
+## Things to improve
+* The code could run faster using threads, so that's the next feature to implement.
 
 ## What I Learned
+* How to download and store images using requests and python file functions.
+* Threading can speed up the script by doing multiple requests at a time.
--- Reddit_image_downloader/imagedl.py
+++ Reddit_image_downloader/imagedl.py
@@ -1,13 +1,12 @@
 #! /usr/bin/env python
 
+from imgurpython import ImgurClient
 from datetime import datetime
 from bs4 import BeautifulSoup
 import praw
 import requests
 import os
 import re
-import sys #for sys.exit(). remove after testing functions
-
 
 def is_valid_image(image_url):
     #check if link is an image by looking at file extension
@@ -28,35 +27,63 @@ def get_extension(image_url):
         if(image_url[len(image_url) - 3:len(image_url)] == extensions[i] or image_url[len(image_url) - 4:len(image_url)] == extensions[i]):
             return '.' + extensions[i]
 
-def album_download(album_page):
-    album_soup = BeautifulSoup(album_page.content, 'html.parser')
-    for image in album_soup.findAll('img'):
-        print(image.get('src'))
+# The album/gallery id would be WDe1xRO when the link is https://imgur.com/gallery/WDe1xRO
+def get_album_id(album_url):
+    for i in range(len(album_url)):
+        if(album_url[i] == '/'):
+            id_start_index = i + 1 #Add one so we don't include the second '/' from /gallery/ or /a/
+    
+    album_id = album_url[id_start_index:len(album_url)]
+    return album_id
+
+#The image id would be Cai5Ore, when the url is https://i.imgur.com/Cai5Ore.jpg
+def get_image_id(image_url):
+    for i in range(len(image_url)):
+        if(image_url[i] == '/'):
+            id_start_index = i + 1 
+    
+    for i in range(id_start_index, len(image_url)):
+        if(image_url[i] == '.'):
+            id_end_index = i
+            break
+    
+    image_id = image_url[id_start_index:id_end_index]
+    return image_id
+
+def album_download(client, album_id):
+    images = client.get_album_images(album_id)
+    for image in images:
+        image_download(image.link)
+
+def image_download(image_url, title=None):
+    if(title == None):
+        title = get_image_id(image_url)
+
+    photo_path = 'test/' + title + get_extension(image_url)
+
+    #This will download an image only when an image doesn't have the same title as a downloaded one
+    if(not os.path.exists(photo_path)): 
+        with open(photo_path, 'wb') as f:
+            f.write(requests.get(image_url).content)
+
+imgur_client_id = '' 
+imgur_client_secret = ''
+
+client = ImgurClient(imgur_client_id, imgur_client_secret)
 
 start = datetime.utcnow()
 
 reddit = praw.Reddit('bot1')
 
-subreddit = reddit.subreddit("lizwenya")
+subreddit = reddit.subreddit("") 
 
 for submission in subreddit.hot(limit=5):
     if(is_valid_image(submission.url)):
-        photo_path = 'test/' + submission.title + get_extension(submission.url)
-
-        #This will download an image only when an image doesn't have the same title as a downloaded one
-        if(not os.path.exists(photo_path)): 
-            with open(photo_path, 'wb') as f:
-                f.write(requests.get(submission.url).content)
+       image_download(submission.url, submission.title);
 
     elif(is_imgur_album(submission.url)):
-        album_page = requests.get(submission.url)
-        album_download(album_page)
+        album_id = get_album_id(submission.url)
+        album_download(client, album_id)
 
 elasped = datetime.utcnow() - start
 print(elasped)
-
-"""
-Expansion ideas:
-Combine with flask to create gallery from links
-Add folders based on name/facial recognition
-"""
