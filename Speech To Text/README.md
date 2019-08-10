e28c8b6f669aec6cbee3bbd7dd6dafb73c064fbf
AlisonWonderland <dimitridawn@gmail.com>
2019-08-10 15:08:29 -0700

    Refactored the code

---
+++ Speech To Text/README.md
@@ -0,0 +1,16 @@
+# Speech to text script
+Script that uses [IBM's Speech to Text API](https://cloud.ibm.com/catalog/services/speech-to-text) to create a speech transcript of audio files stored in the same folder/directory as the script.
+
+API Docs: https://cloud.ibm.com/apidocs/speech-to-text?code=python
+
+<b>Note:</b> You have to add your own API key to 'speech_to_text' for this to work
+
+## Supported file extensions 
+* wav
+* ogg
+* flac
+* mp3
+
+## Example output
+This is with 2 audio files that have the same audio, just different names:
+![Terminal output](https://i.imgur.com/i6IO2M8.png)
--- Speech To Text/stt.py
+++ Speech To Text/stt.py
@@ -4,15 +4,20 @@ import json
 import os
 from ibm_watson import SpeechToTextV1
 
+speech_to_text = SpeechToTextV1(
+    # Add apikey here
+    iam_apikey='',
+    url='https://stream.watsonplatform.net/speech-to-text/api'
+)
+
+
 def searchFiles():
     supported_formats = [".wav", ".flac", ".mp3", ".ogg"]
-   
-    curr_path = os.path.dirname(os.path.abspath(__file__))
-    # Not sure if it looks good, but I'll keep it for now.    
-    return [file for i in range(len(supported_formats)) for file in os.listdir(curr_path) if file.endswith(supported_formats[i])]
+    curr_directory = os.path.dirname(os.path.abspath(__file__))  
+    return [file for i in range(len(supported_formats)) for file in os.listdir(curr_directory) if file.endswith(supported_formats[i])]
 
-def printTranscript():
-    audio_path = os.path.dirname(os.path.abspath(__file__)) + '/audio-file.flac'
+def printTranscript(file):
+    audio_path = os.path.dirname(os.path.abspath(__file__)) + '/' + file
 
     with open(audio_path, 'rb') as audio_file:
         speech_recognition_results = speech_to_text.recognize(
@@ -21,15 +26,16 @@ def printTranscript():
         ).get_result()
 
     transcript = speech_recognition_results["results"][0]["alternatives"][0]["transcript"]
-    print("Transcript: ", transcript)
+    confidence = speech_recognition_results["results"][0]["alternatives"][0]["confidence"] * 100
+    print("\n++++++++++++++++++++++++++++++++++++++", file, "transcript++++++++++++++++++++++++++++++++++++++", end='\n')
+    print("Watson says with", str(confidence) + "%", "confidence that this was said:")
+    print(transcript, end='\n\n')
 
-# Don't worry the key has been deleted :)
-speech_to_text = SpeechToTextV1(
-    iam_apikey='34mk2TEXUBRWjma7q7bdkTVS9jZaJLsB8vDIDcdKE2zu',
-    url='https://stream.watsonplatform.net/speech-to-text/api'
-)
+def main():
+    files = searchFiles()
 
-files = searchFiles()
+    for i in range(len(files)):
+        printTranscript(files[i])
 
-for i in range(len(files)):
-    printTranscript()
+if __name__ == "__main__":
+    main()
